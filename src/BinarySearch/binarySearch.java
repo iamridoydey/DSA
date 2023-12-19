9dabb20e687a20f07197e4da7b3e2cd723355a0c
ridoydey <ridoychandradey10@gmail.com>
2023-12-19 13:19:44 +0600

    Binary Search Algorithm

--- src/BinarySearch/binarySearch.java
+++ src/BinarySearch/binarySearch.java
@@ -1,7 +1,31 @@
 package BinarySearch;
 
+import java.util.Arrays;
+
 public class binarySearch {
     public static void main(String[] args) {
-        System.out.println("I love you Prite.");
+        int[] nums = {1, 5, 7, 9, 12, 13, 18, 20, 21, 29};
+        int ans = BS(nums, 9);
+        System.out.println(ans);
+    }
+
+    static int BS(int[] nums, int target){
+        // set the start and end first
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target){
+                return mid;
+            }
+
+            if (nums[mid] < target){
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
     }
 }
